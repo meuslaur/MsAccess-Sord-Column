e1cf5af8124c66c63ca6644566192ce76e152e8a
Meus Lau <meuslau@protonmail.com>
2022-05-17 17:38:23 +0200

    Delete Csord-Source directory

--- Csord-Source/tbldefs/T_Info.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [T_Info] (
-  [ID_Info] VARCHAR (15) CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
-  [ID_Lang] LONG ,
-  [InfoTitre] VARCHAR (255),
-  [InfoTexte] LONGTEXT ,
-  [Code] LONGTEXT ,
-  [ID_Res] SHORT 
-)
